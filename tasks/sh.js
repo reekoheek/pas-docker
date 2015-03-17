eee06b25047a31b13ee7bdac11be1278af13b175
Ganesha <reekoheek@gmail.com>
2015-03-17 13:25:59 +0700

    fixed sh task to attach to current working shell or using -s, --single if you dont want to attach

--- tasks/sh.js
+++ tasks/sh.js
@@ -13,17 +13,29 @@ var shTask = module.exports = function(name) {
     return new Promise(function(resolve, reject) {
         var containerName = containerManifest.prefixName + '0';
         var imageName = containerManifest.imageName;
+        var cmd;
 
-        console.log(this.opts);
+        if (this.opts.s || this.opts.single) {
+            cmd = spawn('docker', ['run', '-ti', imageName, 'bash'], {stdio:'inherit'});
+            cmd.on('exit', function(code) {
+                if (code === 0 || code === '0') {
+                    this.report('message', 'Exit successfully');
+                    resolve();
+                } else {
+                    reject(new Error('Exit with error code: ' + code));
+                }
+            }.bind(this));
+        } else {
+            cmd = spawn('docker', ['exec', '-ti', containerName, 'bash'], {stdio:'inherit'});
+            cmd.on('exit', function(code) {
+                if (code === 0 || code === '0') {
+                    this.report('message', 'Exit successfully');
+                    resolve();
+                } else {
+                    reject(new Error('Exit with error code: ' + code));
+                }
+            }.bind(this));
+        }
 
-        var cmd = spawn('docker', ['run', '-ti', imageName, 'bash'], {stdio:'inherit'});
-        cmd.on('exit', function(code) {
-            if (code === 0 || code === '0') {
-                this.report('message', 'Exit successfully');
-                resolve();
-            } else {
-                reject(new Error('Exit with error code: ' + code));
-            }
-        }.bind(this));
     }.bind(this));
 };
